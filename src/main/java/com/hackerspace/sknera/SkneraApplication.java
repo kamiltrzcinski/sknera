37f8f55496840c958f7ce970352327e00e2f6e8e
Piotrreeqq <petkepiotr@gmail.com>
2019-10-12 15:54:29 +0200

    first commit

---
+++ src/main/java/com/hackerspace/sknera/SkneraApplication.java
@@ -0,0 +1,13 @@
+package com.hackerspace.sknera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkneraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkneraApplication.class, args);
+	}
+
+}
